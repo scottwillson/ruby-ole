15cd3c4d572ff008d35aff5f896b0cdae13e05b3
Stephen Anderson and Bradley Grzesiak <dev+bendycode+listrophy@bendyworks.com>
2010-03-08 16:34:20 -0600

    Root Name can be "R", too

--- lib/ole/storage/base.rb
+++ lib/ole/storage/base.rb
@@ -147,7 +147,7 @@ module Ole # :nodoc:
 			# silence this warning by default, its not really important (issue #5).
 			# fairly common one appears to be "R" (from office OS X?) which smells
 			# like some kind of UTF16 snafu, but scottwillson also has had some kanji...
-			#Log.warn "root name was #{@root.name.inspect}" unless @root.name == 'Root Entry'
+			Log.warn "root name was #{@root.name.inspect}" unless ['Root Entry', 'R'].include?(@root.name)
 			unused = @dirents.reject(&:idx).length
 			Log.warn "#{unused} unused directories" if unused > 0
 
